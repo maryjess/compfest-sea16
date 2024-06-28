f6dbb7fb61e249103cb896096dfb438441a65413
Jessica Mary <jessicamary472@gmail.com>
2024-06-28 11:06:19 +0800

    Create models for user and admin

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# compfest-sea16
+# Quick Setup Guide
+
+- Clone this repository locally on your device
+  ```
+  git clone https://github.com/maryjess/compfest-sea16.git
+  ```
+- Make sure Python (version?) is installed
+  - Give tutorial on how to install Python if it is not yet installed (setup guide)
+- Setting up virtual environment
+
+  - Create virtual environment if don't have yet (setup guide)
+
+    ```
+    # check if virtualenv is already installed
+    pip list
+
+    # if not installed:
+    pip install virtualenv
+
+    # create virtual env called venv
+    virtualenv .venv
+
+    ```
+
+    - Starting the virtual environment
+      ```
+      source .venv/bin/activate
+      ```
+
+- Make sure all dependencies are installed
+  ```
+  pip install -r requirements.txt
+  ```
+- Running the django server locally
+  ```
+  # by default, the server will run locally at http://127.0.0.1:8000/
+  python manage.py runserver
+  ```
+- Starting the application
+
+  ```
+
+  ```
--- compfest_sea16/compfest/models.py
+++ compfest_sea16/compfest/models.py
@@ -1,6 +1,78 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+# class CustomUser(AbstractBaseUser):
+#     email = models.EmailField(unique=True)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+    
+#     objects = CustomUserManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+#     class Meta:
+#         abstract = True
+
+#     def __str__(self):
+#         return self.email
+
+class UserType(models.TextChoices):
+    CUSTOMER = 'customer', 'Customer'
+    ADMIN = 'admin', 'Admin'
+
+class User(AbstractBaseUser):
+    user_id = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = PhoneNumberField()
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
+
+    type = models.CharField(
+        max_length = 8,
+        choices = UserType.choices,
+        default = UserType.CUSTOMER,
+    )
+    # TODO password will be configured during authentication (django auth)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.email
+
+class Admin(User):
+    is_admin = models.BooleanField(False)
+    type = UserType.ADMIN
+
+class Customer(User):
+    # TODO think whether is_admin is better,
+    # or whether using enumeration is better
+    is_admin = models.BooleanField(False)
+    type = UserType.CUSTOMER
