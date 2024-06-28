a4725dd1e6b06d1ae5e0b74e7ff2759451833148
Jessica Mary <jessicamary472@gmail.com>
2024-06-28 20:14:27 +0800

    Fix login and logout paths

--- compfest_sea16/compfest/urls.py
+++ compfest_sea16/compfest/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('review/', views.review, name='review'),
     path('book_reservation/', views.book_reservation, name='book_reservation'),
-    path('auth/', include('django.contrib.auth.urls')),  # Use built-in authentication views
+    path('login/', views.login, name='login'),
+    path('logged_out/', views.logged_out, name='logged_out'),
 ]
 
 # TODO check out this and see if needed
--- compfest_sea16/compfest/views.py
+++ compfest_sea16/compfest/views.py
@@ -11,3 +11,9 @@ def review(request):
 
 def book_reservation(request):
     return render(request, 'compfest/book_reservation.html')
+
+def login(request):
+    return render(request, 'compfest/login.html')
+
+def logged_out(request):
+    return render(request, 'compfest/logged_out.html')
